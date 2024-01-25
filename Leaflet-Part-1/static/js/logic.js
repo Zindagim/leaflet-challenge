35f1addd9cfbe2288262ed2c3ec772037a8a3c03
Zindagim <benaali1234@gmail.com>
2024-01-25 12:08:20 -0500

    message

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,80 @@
+// Store our API endpoint as queryUrl
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+d3.json(queryUrl).then(function(earthquakeData) {
+   
+    console.log(earthquakeData);
+    createFeatures(earthquakeData.features);
+});
+
+function createMarker(feature, latlng) {
+    return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color:"#000",
+        weight: 0.5,
+        opacity: 0.5,
+        fillOpacity: 1
+    });
+}
+
+function createFeatures(earthquakeData) {
+ 
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place}<h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`);
+    }
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarker
+    });
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+    // Create the base layers
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      });
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [street, earthquakes]
+    });
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap); 
+    
+    let legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 60, 90],
+            labels = [],
+            legendInfo = "<h5>Magnitude</h5>";
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + markerColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }    
+
+        return div;
+
+        };
+
+        // Add legend to map
+        legend.addTo(myMap);
+}
+
+// Increase marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+function markerColor(depth) {
+    return depth > 90 ? '#d73027' :
+            depth > 70 ? '#fc8d59' :
+            depth > 50 ? '#fee08b' :
+            depth > 30 ? '#d9ef8b' :
+            depth > 10 ? '#91cf60' :
+                         '#1a9850' ;          
+}
---
+++ static/js/logic.js
@@ -0,0 +1,107 @@
+// Store our API endpoints as queryUrl and plateUrl
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let plateUrl = "static/data/PB2002_boundaries.json"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(earthquakeData) {
+    d3.json(plateUrl).then(function(plateData) {
+        console.log(earthquakeData);
+        console.log(plateData);
+        createFeatures(earthquakeData.features, plateData.features);
+    });
+});
+function createMarker(feature, latlng) {
+    return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color:"#000",
+        weight: 0.5,
+        opacity: 0.5,
+        fillOpacity: 1
+    });
+}
+
+function createFeatures(earthquakeData, plateData) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place}<h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`);
+    }
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarker
+    });
+    let plates = L.geoJSON(plateData, {
+        style: function() {
+            return {
+                color: "blue",
+                weight: 2.5
+            }
+        }
+    });
+
+    // Send earthquakes layer to the createMap function
+    createMap(earthquakes, plates);
+}
+
+function createMap(earthquakes, plates) {
+    // Create the base layers
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      });
+
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      });
+
+    let baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+    let overlayMaps = {
+        "Earthquakes": earthquakes,
+        "Fault Lines": plates
+    };
+
+    // Create map
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [street, earthquakes, plates]
+    });
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap); 
+    
+    let legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 60, 90],
+            labels = [],
+            legendInfo = "<h5>Magnitude</h5>";
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + markerColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }    
+
+        return div;
+
+        };
+
+        // Add legend to map
+        legend.addTo(myMap);
+}
+
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+function markerColor(depth) {
+    return depth > 90 ? '#d73027' :
+            depth > 70 ? '#fc8d59' :
+            depth > 50 ? '#fee08b' :
+            depth > 30 ? '#d9ef8b' :
+            depth > 10 ? '#91cf60' :
+                         '#1a9850' ;          
+}
